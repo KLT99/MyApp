5f130f1b9198a6099e125cd2e81ee8d2422814e2
KLT <kari9480@gmail.com>
2024-08-22 10:43:10 -0600

    Prueba de uso FrameLayout

---
+++ app/src/main/java/com/klt/myapp/FrameLayout.java
@@ -0,0 +1,32 @@
+package com.klt.myapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FrameLayout extends AppCompatActivity {
+
+    private ImageView iv1;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+
+        iv1 = (ImageView) findViewById(R.id.imageView);
+        button = (Button) findViewById(R.id.btnHidden);
+
+    }
+
+    public void Ocultar(View view){
+
+        button.setVisibility(View.INVISIBLE);
+        iv1.setVisibility(View.VISIBLE);
+
+    }
+
+}
